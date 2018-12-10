84fd53c92227e40bed34e6ae5097de8cb9651ac4
jmpark1115 <jmpark1115@gmail.com>
2018-12-10 23:39:29 +0900

    모두 확인을 위해 all -> null 로 변경

--- coin.py
+++ coin.py
@@ -1,43 +1,26 @@
 import requests
 
-"""
-def coin_val():
-    url = "https://api.bithumb.com/public/ticker/ALL"
-
-    resp = requests.get(url)
-    content = json.loads(resp.content)
-    BTC = content['data']['BTC']['closing_price']
-    ETH = content['data']['ETH']['closing_price']
-    BCH = content['data']['BCH']['closing_price']
-    XRP = content['data']['XRP']['closing_price']
-    return BTC, ETH, BCH, XRP
-    
-"""
-
-
-
 def coin_info(**coin):
-    
     coin_type = coin['coin_name']
     url = "https://api.bithumb.com/public/ticker/ALL"
 
     resp = requests.get(url)
-    content = resp.json()    
-    
+    content = resp.json()
+
     try:
-        if(coin_type != 'ALL'):
+        if coin_type :
             val = content['data'][coin_type]['closing_price']
-            coin_val = "{}({})" .format(coin_type, val)
+            coin_val = "{}({})".format(coin_type, val)
         else:
             BTC = content['data']['BTC']['closing_price']
             ETH = content['data']['ETH']['closing_price']
             BCH = content['data']['BCH']['closing_price']
             XRP = content['data']['XRP']['closing_price']
-            coin_val = "BTC({}) ETH({}) BCH({}) XRP({})" .format(BTC, ETH, BCH, XRP)
+            coin_val = "BTC({}) ETH({}) BCH({}) XRP({})".format(BTC, ETH, BCH, XRP)
     except:
-            coin_val = 'Error'
+        coin_val = 'Error'
     return coin_val
 
 
-if __name__ == '__main__':
-    print(coin_val())
+# if __name__ == '__main__':
+#     print(coin_val())
