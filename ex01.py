95b62a0086aa8a36240aa8980749f5229cf0de86
jmpark1115 <jmpark1115@gmail.com>
2019-04-16 14:50:50 +0000

    url changed

--- ex01.py
+++ ex01.py
@@ -3,7 +3,8 @@ from bs4 import BeautifulSoup
 import requests
 
 #read file from network, zerobizcoin@nate.com
-baseurl = 'http://13.125.232.188/quizbot/ex01'
+baseurl = 'http://ec2-13-125-20-194.ap-northeast-2.compute.amazonaws.com//quizbot/ex01'
+baseurl = 'http://13.125.20.194/quizbot/ex01'
 
 html = requests.get(baseurl).text
 soup = BeautifulSoup(html, 'lxml')
